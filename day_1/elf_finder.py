88f59d73c743edd83a6004de26c19796f4e9bd72
Andrew Evans <aevans@phdata.io>
2022-12-01 14:19:52 -0600

    part_2

--- day_1/elf_finder.py
+++ day_1/elf_finder.py
@@ -1,14 +1,33 @@
 from data_loader import data_loader
 
 
-raw_data = data_loader(day=1, year=2022)
+def part_1():
+    raw_data = data_loader(day=1, year=2022)
 
-best_elf_calories = 0
-for elf in raw_data.split('\n\n'):
+    best_elf_calories = 0
+    for elf in raw_data.split('\n\n'):
+        elf_snack_calories = elf_calories(elf)
+        if elf_snack_calories > best_elf_calories:
+            best_elf_calories = elf_snack_calories
+
+    print(best_elf_calories)
+    return best_elf_calories
+
+
+def part_2():
+    raw_data = data_loader(day=1, year=2022)
+
+    elves_calories = [elf_calories(elf) for elf in raw_data.split('\n\n')]
+
+    elves_calories.sort()
+    top_3 = sum(elves_calories[-3:])
+
+    print(top_3)
+    return top_3
+
+
+def elf_calories(elf):
     elf_snack_calories = 0
     for snack in elf.split('\n'):
         elf_snack_calories += int(snack)
-    if elf_snack_calories > best_elf_calories:
-        best_elf_calories = elf_snack_calories
-
-print(best_elf_calories)
+    return elf_snack_calories
